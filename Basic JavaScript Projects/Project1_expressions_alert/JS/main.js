641e4eeda0e304bd6fe6b8b0194110bd9d1b51df
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-06-13 20:56:00 -0500

    Basic JavaScript Projects Submission
    Submission of initial JavaScript challenges acquainting us with basic JavaScript functionality, including the following:
    
    - FOLDER STRUCTURE ASSIGNMENT - Step 36
    - SCRIPT ASSIGNMENT - Step 38
    - WINDOW.ALERT() METHOD ASSIGNMENT - Step 40
    - DOCUMENT.WRITE() METHOD ASSIGNMENT - Step 42
    - ASSIGN VARIABLE TO STRING + DOCUMENT.WRITE() - Step 46
    - STRING ASSIGNMENT - Step 49
    - ESCAPE CHARACTER ASSIGNMENT - Step 51
    - CONCATENATION ASSIGNMENT - Step 53
    - CONCATENATED STRING CHALLENGE - Step 54
    - MULTIPLE VARIABLES ASSIGNMENT - Step 56
    - COLORS ASSIGNMENT - Step 58
    - EXPRESSION ASSIGNMENT - Step 60
    - COMMENT ASSIGNMENT - 65

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,38 @@
+var A = "Porsche "; //Assigned variable
+document.write(A);  //Statement calling variable
+
+document.write("<br><br>")  //Inserted a blank line
+
+window.alert("Porsche Website") //Window alert to grab user attention
+
+document.write("Janis Joplin once said, \"Oh, Lord, won\'t you buy me a Mercedes Benz - My friends all drive Porsches, I must make amends.\""); //Statment calling variable that includes both an apostrophe and quotations marks using escape characters
+
+document.write("<br><br>")  //Inserted a blank line
+
+document.write("Janis Joplin once said,"    //Statment calling variable that includes both an apostrophe and quotations marks using escape characters
++ " \"Oh, Lord, won\'t you buy me a Mercedes Benz - My friends all drive"   //Concatenated quotation
++ " Porsches, I must make amends.\"");  //Concatenated quotation
+
+document.write("<br><br>")  //Inserted a blank line
+
+var C = "Porsche" + " 718 Boxster GTS 4.0"; //Assigned variable + concatenated the make and model
+document.write(C)   //Statement calling variable
+
+document.write("<br><br>")  //Inserted a blank line
+
+var AutoFamily = "Porsche", His = "911", Hers = "Cayenne";  //Assigned multilple variables in one statement
+document.write(Hers)    //Statement calling variable
+
+document.write("<br><br>")  //Inserted a blank line
+
+var hers = "Hers";  //Assigned variable
+var hers = hers.fontcolor("blue");  //Attributed color
+document.write(hers);   //Statement calling variable
+
+document.write("<br><br>")  //Inserted a blank line
+
+var s, t, u;        //Assigned variables for an expression
+x = 350;            //Assigned value for "x"
+y = 650;            //Assigned a value for "y"
+z = x + y;          //Assigned a value for "z"
+document.write(z)  //Statement calling an expression
