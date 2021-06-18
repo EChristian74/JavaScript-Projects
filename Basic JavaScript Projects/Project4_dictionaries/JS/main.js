db024d3a02c79716d7523fbe87e8048c26d74dd9
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-06-18 11:26:03 -0500

    Project4_dictionaries Submission
    Submission of fourth round of JavaScript challenges acquainting us with JavaScript functions, including the following:
    
    * DICTIONARY ASSIGNMENT: Create project folder, index.html, JS folder, and main.js - Step 100
    * DICTIONARY CHALLENGE: Test duplicate keys - Step 101
    * DELETE ASSIGNMENT: Test delete operator - Step 103
    * JavaScript Commenting
    * Submitted 6/18/2021

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,13 @@
+function car_Function() {   //Function to create dictionary
+    var Car =   {   //Variable to define dictionary
+        Class:"Sports Car",   //Define KVP 1
+        Make:"De Tomaso Automobili",    //Define KVP 2
+        Model:"Pantera",    //Define KVP 3
+        Body:"2-door coupe",    //Define KVP 4
+        Engine:"Ford 302 V8",   //Define KVP 5 
+        Engine:"Ford 427",   //Define KVP 6 to test duplicate keys - tested code with and without this key/pair
+        Transmission:"5-speed manual"   //Define KVP 7
+    };       
+    delete Car.Engine;  //Delete operation
+    document.getElementById("Dictionary").innerHTML = Car.Make;   //Method to output value
+}
