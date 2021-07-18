c18b023f4fbc2dcb67976e7371fd52e018f4c3c5
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-07-18 14:18:57 -0500

    VW UNIT CHALLENGE
    Series of exercises using site with HTML, CSS and JavaScript to prepare for updating portfolio site.

---
+++ Basic JavaScript Projects/VW Unit Challenge/js/vw.js
@@ -0,0 +1,42 @@
+// removes fade-out
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.remove('fade-out');
+  });
+
+// form validation
+function validateForm() {
+    let x = document.forms["vwForm"]["phone"].value;
+    if (x == "") {
+        alert("Please provide phone number");
+        return false;
+    }
+}
+
+function openForm() {
+    document.getElementById("popForm").style.display = "block";
+}
+
+    function closeForm() {
+    document.getElementById("popForm").style.display = "none";
+}
+
+// automatic slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}  
+    slides[slideIndex-1].style.display = "block";  
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+   
